04255e55a09c1b313a55817751cde2787928e519
s-alex-smith <58820910+s-alex-smith@users.noreply.github.com>
2021-07-22 16:23:08 +0100

    first unit test

---
+++ WebApi/Controllers/ElephantController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet("greeting")]
+        public string GetString()
+        {
+            return "I hope this greeting message is being read!";
+        }
+
+        [HttpGet("all")]
+        public async Task<List<Elephant>> GetElephantList()
+        {
+            var elephantFilePath = await System.IO.File.ReadAllTextAsync(@"elephants.json");
+            var elephantData = JsonSerializer.Deserialize<List<Elephant>>(elephantFilePath);
+            return elephantData;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Elephant> GetElephantById(Guid id)
+        {
+            var elephantList = await GetElephantList();
+            return elephantList.Find(elephant => elephant.id == id);
+        }
+    }
+}
---
+++ WebApiTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using WebApi;
+using Xunit;
+using WebApi.Controllers;
+using System.Collections.Generic;
+
+namespace WebApiTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetGreetingReturnsNonEmptyString()
+        {
+            var testGreetingController = new GreetingController();
+            var testData = testGreetingController.GetString();
+
+            Assert.True(testData.Length > 0);
+        }
+
+        [Fact]
+        public async void GetAllElephantsReturnsNonEmptyList()
+        {
+            var elephant1 = new Elephant();
+            var elephant2 = new Elephant();
+            var listOfElephants = new List<Elephant>()
+            {
+                elephant1,
+                elephant2
+            };
+
+            var testGreetingController = new GreetingController();
+            var testElephantData = await testGreetingController.GetElephantList();
+
+            Assert.True(testElephantData.Count > 0);
+        }
+    }
+}
